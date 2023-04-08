be16b581a542c8e595eb0c910024613f52a8eb1c
Ahmed Yamany <yamany2862016@gmail.com>
2023-04-08 10:07:14 +0200

    Initial Commit

---
+++ E-Commerce SwiftUI/E_Commerce_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_Commerce_SwiftUIApp.swift
+//  E-Commerce SwiftUI
+//
+//  Created by Ahmed Yamany on 08/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct E_Commerce_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
